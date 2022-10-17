6d5b17db46fa6ca52056f41f104abd058f24d6ad
Danieljacon <danielcamillo2020@gmail.com>
2022-10-17 20:32:19 -0300

    Exercício 7 adicionado.

---
+++ homeworks/h1/exercicios/e7/script.js
@@ -0,0 +1,13 @@
+function fezPegadinha() {
+    alert("Tenta de novo! 1");
+    alert("Tenta de novo! 2");
+    alert("Tenta de novo! 3");
+    alert("Tenta de novo! 4");
+    alert("Tenta de novo! 5");
+    alert("Tenta de novo! 6");
+    alert("Só falta mais um :( 7");
+    alert("Tá cansada anja?! 8");
+    alert("Finalmente amiga");
+}
+
+fezPegadinha();
