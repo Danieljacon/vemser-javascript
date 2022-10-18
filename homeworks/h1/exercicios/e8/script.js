4b433d4a48ee76ce2d48ee759a07dec29fc06c85
Danieljacon <danielcamillo2020@gmail.com>
2022-10-18 10:41:15 -0300

    Modificado o exercício 8 para remover o objeto.

--- homeworks/h1/exercicios/e8/script.js
+++ homeworks/h1/exercicios/e8/script.js
@@ -6,18 +6,42 @@ function calculadora(num1, operation, num2) {
   num1 = parseInt(num1);
   num2 = parseInt(num2);
 
-  const operacoes = {
-    "+": num1 + num2,
-    "-": num1 - num2,
-    "*": num1 * num2,
-    "/": num1 / num2,
-  };
+  function calculos() {
+    return operation === "+"
+      ? num1 + num2
+      : operation === "-"
+      ? num1 - num2
+      : operation === "*"
+      ? num1 * num2
+      : operation === "/"
+      ? num1 / num2
+      : "Operação inválida";
+  }
 
-  return !operacoes[operation]
-    ? "Operação inválida"
-    : num2 === 0 && operation === "/"
-    ? "Não é possível dividir por zero"
-    : operacoes[operation];
+  function calculosIncorretos() {
+    return (
+      (operation !== "+" &&
+        operation !== "-" &&
+        operation !== "*" &&
+        operation !== "/") ||
+      (num2 === 0 && operation === "/")
+    );
+  }
+
+  return calculosIncorretos() ? "Operação inválida ou dividido por zero" : calculos();
 }
 
 alert(calculadora(num1, operation, num2));
+
+// const operacoes = {
+//   "+": num1 + num2,
+//   "-": num1 - num2,
+//   "*": num1 * num2,
+//   "/": num1 / num2,
+// };
+
+// return !operacoes[operation]
+//   ? "Operação inválida"
+//   : num2 === 0 && operation === "/"
+//   ? "Não é possível dividir por zero"
+//   : operacoes[operation];
