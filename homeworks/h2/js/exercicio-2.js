2d6c31c09a1240a038cf340120daaa157a00c143
Danieljacon <danielcamillo2020@gmail.com>
2022-10-18 19:18:09 -0300

    Exercicio 2 feito.

--- homeworks/h2/js/exercicio-2.js
+++ homeworks/h2/js/exercicio-2.js
@@ -1,4 +1,8 @@
 const exercicio2 = () => {
-    const textToChange = selectHtml("e1-new-text");
-    console.log(textToChange);
+    const textToChange = selectHtml("e2-text-color");
+    const inputChangeColor = selectHtml("e2-input-change-color");
+
+    inputChangeColor.addEventListener("input", () => {
+        textToChange.style.color = inputChangeColor.value;
+    });
 }
