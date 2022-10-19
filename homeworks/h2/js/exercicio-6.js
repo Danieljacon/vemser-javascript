7c6513a2e5c32fbb02807460774ef484ec1f8d55
Danieljacon <danielcamillo2020@gmail.com>
2022-10-18 21:57:11 -0300

    Javascript do exercício 6 (com eval)

--- homeworks/h2/js/exercicio-6.js
+++ homeworks/h2/js/exercicio-6.js
@@ -1,4 +1,17 @@
-const exercicio6 = () => {
-    const result = document.getElementById("e6-result").value;
-    console.log(result);
-}
+const resultadoTela = document.getElementById("e6-result");
+
+const calcular = (value) => {
+  return (resultadoTela.value = resultadoTela.value + value);
+};
+
+const resultado = () => {
+  return (resultadoTela.value = eval(resultadoTela.value));
+};
+
+const deletar = () => {
+  return (resultadoTela.value = resultadoTela.value.slice(0, -1));
+};
+
+const limpar = () => {
+  return (resultadoTela.value = "");
+};
