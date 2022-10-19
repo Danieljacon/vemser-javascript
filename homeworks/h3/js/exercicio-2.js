beb06d664069c8c68d4e89c284b49e5a57dd2982
Danieljacon <danielcamillo2020@gmail.com>
2022-10-19 19:37:37 -0300

    Exercicio 3 feito.

---
+++ homeworks/h3/js/exercicio-2.js
@@ -0,0 +1,17 @@
+const resultado = document.getElementById("e2-resultado");
+let pares = [];
+let somaTodosOsPares = 0;
+
+for (let i = 0; i <= 50; i++) {
+  i % 2 === 0 && (pares = [...pares, i]);
+}
+
+const somarItensArray = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    somaTodosOsPares += array[i];
+  }
+  return somaTodosOsPares;
+};
+
+somarItensArray(pares);
+resultado.innerHTML = `A soma de todos os números pares entre 0 e 50 é <h3 class="text-danger">${somaTodosOsPares}</h3>`;
