b2b6c7567b7e1ca19af3f40c77727035af89f2a9
Danieljacon <danielcamillo2020@gmail.com>
2022-10-20 17:16:48 -0300

    Feito o exercício 2.

---
+++ homeworks/h4/js/exercicio-2.js
@@ -0,0 +1,15 @@
+const exercicio2 = () => {
+    const arrayInicial = document.getElementById("e2-arrayInicial");
+    const arrayPar = document.getElementById("e2-arrayPar");
+  const array = [];
+  for (let i = 0; i < 30; i++) {
+    array.push(i + 1);
+    arrayInicial.innerHTML += `${i + 1} `;
+
+    if (array[i] % 2 === 0) {
+        array[i] = `${array[i]} é par, indice ${i}`;
+        arrayPar.innerHTML += `${array[i]}<br>`;
+    }
+  }
+  console.log(array);
+};
