20aba94fa393d01ccb4fe404255dc8ad0d7fe7ef
Danieljacon <danielcamillo2020@gmail.com>
2022-10-20 00:18:08 -0300

    Feito o exercicio 8

---
+++ homeworks/h3/js/exercicio-8.js
@@ -0,0 +1,29 @@
+const displayAntes2017 = document.getElementById("s8-antes2017");
+const displayDepois2017 = document.getElementById("s8-depois2017");
+
+const salarioInicial = 1000;
+const ajusteAntes2017 = 1.5;
+const anoContratacao = 2013;
+const anoAtual = 2022;
+
+const aumentoAte2017 = () => {
+  let salario = salarioInicial;
+  for (let i = anoContratacao; i <= 2016; i++) {
+    salario = (salario * (ajusteAntes2017 / 100 + 1)).toFixed(2);
+  }
+  displayAntes2017.innerHTML = `R$${salario}`;
+  return salario;
+};
+
+const aumentoDepois2017 = () => {
+  let salario = aumentoAte2017();
+  let ajuste = ajusteAntes2017;
+  for (let i = 2017; i <= anoAtual; i++) {
+    ajuste = ajuste * 2;
+    salario = (salario * (ajuste / 100 + 1)).toFixed(2);
+  }
+  displayDepois2017.innerHTML = `R$${salario}`;
+};
+
+aumentoDepois2017();
+aumentoAte2017();
