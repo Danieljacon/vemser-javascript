6166d35542c30b47b36bd15583c77b499db2a30b
Danieljacon <danielcamillo2020@gmail.com>
2022-10-25 20:43:17 -0300

    Feito o HTML da sessão de comentarios

--- homeworks/h5/js/script.js
+++ homeworks/h5/js/script.js
@@ -1,20 +1,13 @@
-// https://jsonplaceholder.typicode.com/photos
 const cards = document.getElementById("cards");
 const apiUrl = "https://jsonplaceholder.typicode.com/photos?_limit=10";
-
-/*
-                <div class="card">
-                    <img src="https://via.placeholder.com/150/92c952">
-                    <div>
-                        <button class="btn"><i class="fa-regular fa-comment"></i>Comentários</button>
-                        <p>accusamus beatae ad facilis cum similique qui sunt</p>
-                    </div>
-                </div>
-*/
 
 const getCards = async () => {
   const response = await fetch(apiUrl);
   const data = await response.json();
+
+  const cardSelected = (e) => {
+    const card = e.target;
+  };
 
   try {
     if (data !== null) {
@@ -23,15 +16,22 @@ const getCards = async () => {
                 <div class="card">
                     <img src="${e.thumbnailUrl}">
                     <div>
-                        <button class="btn"><i class="fa-regular fa-comment"></i>Comentários</button>
+                        <button class="btn" id="card-${e.id}"><i class="fa-regular fa-comment"></i>Comentários</button>
                         <p>${e.title}</p>
                     </div>
                 </div>
             `;
       });
+    //   document.getElementById("cards").addEventListener("click", cardSelected);
+    } else {
+      cards.innerHTML = `
+        <h1>Houve algum problema!</h1>
+    `;
     }
   } catch (err) {
-    console.log(err);
+    cards.innerHTML = `
+        <h1>Houve algum problema!</h1>
+    `;
   }
 };
 
