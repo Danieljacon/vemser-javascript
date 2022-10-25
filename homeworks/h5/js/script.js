d1140a4c5558eba5bc2b1c2c818b23aaa07111e7
Danieljacon <danielcamillo2020@gmail.com>
2022-10-25 20:57:38 -0300

    Mudando a imagem de acordo com o post

--- homeworks/h5/js/script.js
+++ homeworks/h5/js/script.js
@@ -1,7 +1,28 @@
 const cards = document.getElementById("cards");
+const comments = document.getElementById("comments");
+const cardImage = document.getElementById("card-image");
 const formComments = document.getElementById("form-comments");
-const apiUrl = "https://jsonplaceholder.typicode.com/photos?_limit=10";
+const inputEmail = document.getElementById("input-email");
+const inputMessage = document.getElementById("input-mensagem");
+const apiUrl = "https://jsonplaceholder.typicode.com/photos?_limit=3";
 let id = 1;
+
+const addComments = (e) => {
+  e.preventDefault();
+  const email = inputEmail.value;
+  const message = inputMessage.value;
+
+  if (message !== "") {
+    comments.innerHTML += `
+                <div>
+                    <small><i class="fa-regular fa-envelope"></i>${email.value}</small>
+                    <p>${message.value}</p>
+                </div>
+            `;
+  } else {
+    alert("Preencha os campos");
+  }
+};
 
 const getCards = async () => {
   const response = await fetch(apiUrl);
@@ -10,7 +31,12 @@ const getCards = async () => {
   const cardSelected = (e) => {
     const card = e.target;
     id = parseInt(card.getAttribute("data-id"));
-    console.log(id);
+    // console.log(id);
+
+    const cardSelected = data.filter((item) => item.id === id);
+    console.log(cardSelected);
+    // cardImage change image
+    cardImage.src = cardSelected[0].url;
   };
 
   try {
@@ -32,7 +58,7 @@ const getCards = async () => {
         e.addEventListener("click", cardSelected);
       });
     } else {
-        throw ("Não foi possível carregar os dados");
+      throw "Não foi possível carregar os dados";
     }
   } catch (err) {
     cards.innerHTML = `
